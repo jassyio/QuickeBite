0529d45b1de9c6dd850c4d62c374fc5b738bd9cc
jassyio <joejassyio@gmail.com>
2024-10-21 21:03:12 +0300

    init

--- frontend/src/components/HomePage/Homepage.js
+++ frontend/src/components/HomePage/Homepage.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './Homepage.css';
 
-export default function Homepage() {
+import React from 'react';
+import './Homepage.css'; // Import the CSS file
+
+const Homepage = () => {
   return (
-     <div className="homepage">
-    <h1>QuickBite</h1>
-    <p>Welcome to QuickBite, your go-to destination for quick and easy meal ideas. Whether you're looking for a quick snack or a quick meal, we've got you covered.</p>
+    <div className="hero">
+      <div className="container">
+        <h1>QuickBite</h1>
+        <p>Welcome to QuickBite, your go-to destination for quick and easy meal ideas. Whether you're looking for a quick snack or a quick meal, we've got you covered.</p>
+      </div>
     </div>
   );
-}
+};
+
+export default Homepage;
--- frontend/src/components/NavBar/Navbar.js
+++ frontend/src/components/NavBar/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import './Navbar.css';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file
 
-export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <li><Link to="/"><h1>QuickBite</h1></Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><Link to="/restaurants">Restaurants</Link></li>
-            <li><Link to="/orders">Orders</Link></li>
-        </nav>
-    )
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">QuickBite</Link>
+      </div>
+      <ul>
+        <li><Link to="/restaurants">Restaurants</Link></li>
+        <li><Link to="/order">Order</Link></li>
+        <li><Link to="/track">Track</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
